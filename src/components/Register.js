a9a863ffa073b4bd06f8f91b8efe1565ef7ca8b4
kisakyegordon <gordon56692@gmail.com>
2017-11-28 20:41:18 +0300

    Added a connection to the store

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
+import { PropTypes } from 'prop-types';
 import RegisterForm from '../components/forms/registerform';
+import { connect } from 'react-redux';
+import { registerRequest } from '../actions/registerActions';
 
 class Register extends Component{
     render(){
+
+        const { registerRequest } = this.props;
         return (
+            
             <div>
             <h2> Register </h2>
-            <RegisterForm />
+            <RegisterForm registerRequest={ registerRequest }/>
             </div>
         );
     }
 }
 
-export default Register;
+Register.propTypes = {
+    registerRequest: PropTypes.func.isRequired
+}
+
+export default connect(null, { registerRequest }) (Register);
